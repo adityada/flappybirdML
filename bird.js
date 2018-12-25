e0fd6832560d3b0f2f62fad7d1c402f555492ad5
jargonide <jargonide@gmail.com>
2018-12-25 20:16:07 +0700

    Done!

--- bird.js
+++ bird.js
@@ -1,25 +1,35 @@
-function Bird() {
-    this.y = height/2;
-    this.x = width/4;
+class Bird {
+    constructor(brain) {
+        this.y = height/2;
+        this.x = width/4;
+    
+        this.gravity = 0.8;
+        this.lift = -25;
+        this.velocity = 0;
+        this.airResistance = 0.9;
+        this.jump = false;
+    
+        this.score = 0;
+        this.fitness = 0;
+        if(!brain) {
+            this.brain = new NeuralNetwork(4, 4, 2);
+        } else {
+            this.brain = brain.copy();
+        }
+    }
 
-    this.gravity = 0.8;
-    this.lift = -25;
-    this.velocity = 0;
-    this.airResistance = 0.9;
-    this.data = []
-    this.jump = false;
-    this.data_id = 307;
 
-    this.show = function() {
-        fill(255)
+    show() {
+        fill(255, 50)
+        stroke(255)
         ellipse(this.x, this.y, 32, 32)
     }
 
-    this.up = function() {
+    up() {
         this.velocity += this.lift;
     }
 
-    this.within = function(pipe1, pipe2) {
+    within(pipe1, pipe2) {
         targetPos = []
         for(var i = this.x; i < this.x + Math.floor(pipe1.calcDist(pipe2)/6); i++) {
             //postTarget(this.x, pipe1.calcCoordinate(pipe2, i), this.data_id)
@@ -34,8 +44,38 @@ function Bird() {
         this.data_id++;
     }
 
+    think(pipes) {
+        let inputs = [];
+
+        // Find the closest pipe
+        let closest = null;
+        let closestD = Infinity;
+        for(let i = 0; i < pipes.length; i++) {
+            let d = pipes[i].x - this.x;
+            if(d < closestD && d > 0) {
+                closest = pipes[i]
+                closestD = d;
+            }
+        }
+        inputs[0] = this.y / height;
+        inputs[1] = closest.top / height;
+        inputs[2] = closest.bottom / height;
+        inputs[3] = closest.x / width;
+
+        let outputs = this.brain.predict(inputs);
+        if(outputs[0] > outputs[1]) {
+            this.up();
+        }
+    }
+
+    mutate() {
+        this.brain.copy();
+    }
+
     // Applies gravity
-    this.update = function() {
+    update() {
+        this.score++;
+
         this.velocity += this.gravity;
         this.velocity *= this.airResistance;
         this.y += this.velocity;
--- sketch.js
+++ sketch.js
@@ -1,73 +1,64 @@
-var bird;
+const TOTAL_POPULATION = 350;
+
+let birds = []
+let savedBirds = []
 var pipes = []
-var birdDead = false;
 var data = []
-var hasDied = false;
+let counter = 0;
+let slider;
 
 function setup() {
   createCanvas(400, 600);
-  bird = new Bird();
+  slider = createSlider(1, 100, 1)
+  for(let i = 0; i < TOTAL_POPULATION; i++) {
+    birds[i] = new Bird();
+  }
   pipes.push(new Pipe())
 }
 
 function draw() {
-  background(0);
-  for (var i = pipes.length-1; i >= 0; i--) {
-    pipes[i].show();
-    if (!birdDead) {
+
+  for(let n = 0; n < slider.value(); n++) {
+    if(counter % 80 == 0) {
+      pipes.push(new Pipe())
+    }
+    counter++;
+    for (var i = pipes.length-1; i >= 0; i--) {
+      pipes[i].show();
       pipes[i].update();
-    }
-    if(pipes[i].hits(bird) && !birdDead) {
-      print("TEST!")
-      birdDead = true;
-      hasDied = true;
-    }
-    if(pipes[i].offscreen()) {
-      pipes.splice(i, 1);
-    }
-    if(pipes[i+1] != undefined) {
-      pipes[i].lineConnect(pipes[i+1])
-      if(bird.x < pipes[i+1].x && bird.x > pipes[i].x && !birdDead) {
-        bird.within(pipes[i], pipes[i+1])
+      for(let j = birds.length - 1; j >= 0; j--) {
+        if(pipes[i].hits(birds[j])) {
+          savedBirds.push(birds.splice(j, 1)[0])
+        }
+    
+      }
+      if(pipes[i].offscreen()) {
+        pipes.splice(i, 1);
       }
     }
-  }
-  
-  bird.show();
-  bird.update();
-
-  if(frameCount % 80 == 0) {
-    pipes.push(new Pipe())
-  }
-
-
-if(hasDied) {
-  print("DEAD!")
-  var urlData = "http://127.0.0.1:5000/data";
-  var urlTarget = "http://127.0.0.1:5000/target";
-  for(var i = 0; i < bird.data.length; i++) {
-    for(var j = 0; j < bird.data[i][1].length; j++) {
-      var http = new XMLHttpRequest();
-      http.open("POST", urlTarget, true)
-      http.setRequestHeader("Content-type", "application/json;charset=UTF-8");
-      http.send(JSON.stringify({"xCor": bird.data[i][1][j][0], 
-                                "yCor": bird.data[i][1][j][1], 
-                                "data_id": bird.data[i][1][j][2]}));
+    
+    for(let bird of birds) {
+      bird.think(pipes);
+      bird.update();
     }
-    var http = new XMLHttpRequest();
-    http.open("POST", urlData, true)
-    http.setRequestHeader("Content-type", "application/json;charset=UTF-8");
-    http.send(JSON.stringify({"birdYPos": bird.data[i][0], "jump": bird.data[i][2]}));
-  }
-  hasDied = false;
-}
   
-}
-
-function keyPressed() {
-  // If space bar pressed
-  if (key == ' ') {
-    bird.jump = true;
-    bird.up()
+    if(birds.length == 0) {
+      nextGeneration()
+      pipes = []
+      counter = 0;
+    }
+  
   }
+
+  background(0);
+
+  for(let bird of birds) {
+    bird.show();
+  }
+
+  for(let pipe of pipes) {
+    pipe.show();
+  }
+
+
 }
